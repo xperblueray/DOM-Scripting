24836bb908fe3c4db25446f29aa8229f02f4d70e
XperBlueray <wxb2lp@163.com>
2015-06-30 10:09:18 +0800

    The final version

--- scripts/prepareSlideshow.js
+++ scripts/prepareSlideshow.js
@@ -2,12 +2,20 @@ function prepareSlideshow() {
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById) return false;
     if (!document.getElementById("linklist")) return false;
-    if (!document.getElementById("preview")) return false;
-    var preview = document.getElementById("preview");
-    preview.style.position = "absolute";
-    preview.style.left = "0px";
-    preview.style.top = "0px";
+    //if (!document.getElementById("preview")) return false;
+    var slideshow = document.createElement("div");
+    slideshow.setAttribute("id","slideshow");
+    var preview = document.createElement("img");
+    preview.setAttribute("src","images/topics.gif");
+    preview.setAttribute("alt","building blocks of web design");
+    preview.setAttribute("id","preview")
+    slideshow.appendChild(preview);
     var list = document.getElementById("linklist");
+    insertAfter(slideshow,list);
+
+    //var preview = document.getElementById("preview");
+    //preview.style.position = "absolute";
+    //var list = document.getElementById("linklist");
     var links = list.getElementsByTagName("a");
     links[0].onmouseover = function() {
         moveElement("preview",-100,0,10);
